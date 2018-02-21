6019db178295caad402edd8fb1921b3b6d436eb1
Terran Blake <terranblake@ksu.edu>
2018-02-21 15:41:53 -0600

    Implemented iterator functions for Stack and Queue data structures

--- queue.py
+++ queue.py
@@ -6,9 +6,21 @@ class Queue(object):
     def __init__(self):
         self.list = LinkedList()
         self.last = Node()
+        self.iter = None
 
     def is_empty(self):
         return self.list.head is None
+
+    def __next__(self):
+        if self.has_next():
+            current = self.iter
+            self.iter = self.iter.get_next()
+            return current.get_data()
+        else:
+            raise ValueError('The end of the Stack has been reached!')
+
+    def has_next(self):
+        return self.iter
 
     def dequeue(self):
         if self.list.head:
@@ -16,7 +28,7 @@ class Queue(object):
             self.list.head = self.list.head.get_next()
             return result.get_data()
         else:
-            print('Queue is empty!')
+            raise ValueError('\nNo elements in the Queue!')
 
     def enqueue(self, new_data):
         new_last = Node(new_data)
@@ -24,6 +36,21 @@ class Queue(object):
         if self.list.head is None:
             self.list.head = new_last
             self.last = new_last
-        
+
         self.last.set_next(new_last)
         self.last = new_last
+
+        self.iter = self.list.head
+
+
+def main():
+    myQueue = Queue()
+
+    for word in 'To be or not to be, that is the question'.split(' '):
+        myQueue.enqueue(word)
+
+    print(myQueue.__next__())
+
+
+if __name__ == '__main__':
+    main()
--- stack.py
+++ stack.py
@@ -5,9 +5,22 @@ from node import Node
 class Stack(object):
     def __init__(self):
         self.list = LinkedList()
+        self.list.head = None
+        self.iter = None
 
     def is_empty(self):
-        return self.list.head is None
+        return self.current.get_next()
+
+    def __next__(self):
+        if self.has_next():
+            current = self.iter
+            self.iter = self.iter.get_next()
+            return current.get_data()
+        else:
+            raise ValueError('The end of the Stack has been reached!')
+
+    def has_next(self):
+        return self.iter
 
     def pop(self):
         if self.list.head:
@@ -15,9 +28,23 @@ class Stack(object):
             self.list.head = self.list.head.get_next()
             return result.get_data()
         else:
-            print('Stack is empty!')
+            raise ValueError('No elements in the Stack!')
 
     def push(self, new_data):
-        current = self.list.head
+        self.current = self.list.head
         self.list.head = Node(new_data)
-        self.list.head.set_next(current)
+        self.list.head.set_next(self.current)
+        self.iter = self.list.head
+
+
+def main():
+    myStack = Stack()
+
+    for word in 'To be or not to be, that is the question?'.split(' '):
+        myStack.push(word)
+
+    print(myStack.__next__())
+
+
+if __name__ == '__main__':
+    main()
