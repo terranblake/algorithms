ff8da322565cfad70c3af0d2868e40bcf5ea323e
Terran Blake <terranblake@ksu.edu>
2018-02-28 19:52:37 -0600

    Added basic hash table data structure

---
+++ data_structures/hashtable.py
@@ -0,0 +1,62 @@
+from node import Node
+
+class HashTable(object):
+    def __init__(self, size=None):
+        self.table = {}
+
+        if size and size > 1:
+            self.size = size
+
+    # Call basic_hash(self, result), insert { hash: value }, return hashed equivalent of value
+    def insert(self, value):
+        if value:
+            hashed = self.basic_hash(value)
+            self.table[hashed] = value
+        
+            return {value: hashed}
+
+        raise ValueError('Please provide something to input.')
+
+    # Call search(self, value), delete and return hashed equivalent
+    def delete(self, value):
+        self.table[self.basic_hash(value)] = None
+
+    # If value exists, return hashed equivalent, else ValueError
+    def search(self, value):
+        hashed = self.basic_hash(value)
+
+        for idx, elm in self.table.items():
+            if idx == hashed:
+                return idx
+            
+        raise ValueError('Provided input not found in table.')
+
+    # Hash value, return result of hashing
+    def basic_hash(self, value):
+        return sum([ord(x) for x in value]) % self.size
+
+    def print(self):
+        return self.table
+
+def main():
+
+    myTable = HashTable(10)
+
+    # Test insert function
+    myTable.insert('John Doe')
+    myTable.insert('Jane Doe')
+    myTable.insert('Jackson Johnson')
+    myTable.insert('Matthew Matthews')
+    # myTable.insert('') # Should return error
+
+    # Test delete function
+    myTable.delete('Jane Doe')
+
+    # Test search function
+    myTable.search('Jackson Johnson')
+    myTable.search('Jane Doe') # Should return error, since this does not exist
+
+    print(myTable.print())
+
+if __name__ == '__main__':
+    main()
